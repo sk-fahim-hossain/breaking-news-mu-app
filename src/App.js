01d5c8848f91f4e9f6513c1298ddcaaf9157f40d
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2021-04-28 01:55:02 +0600

    this is the another news site after hot news

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-03-26&sortBy=publishedAt&apiKey=9f617a4bad5747349dfe928f7ca2b8f5'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      {
+        news.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
